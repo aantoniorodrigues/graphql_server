81cc106c890f6041cb74908aad8e28dbf0b6f2e6
Antonio Rodrigues <antoniocvpr@gmail.com>
2025-08-12 12:39:55 +0100

    Add related data resolvers

--- apollo_server/src/resolvers.ts
+++ apollo_server/src/resolvers.ts
@@ -20,6 +20,24 @@ const resolvers = {
         review(_parent, args) {
             return db.reviews.find((review) => review.id === args.id);
         }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.game_id === parent.id);
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.author_id === parent.id);
+        }
+    },
+    Review: {
+        game(parent) {
+            return db.games.find((game) => game.id === parent.game_id);
+        },
+        author(parent) {
+            return db.authors.find((author) => author.id === parent.author_id)
+        }
     }
 }
 
